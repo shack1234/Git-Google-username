38daf029b5c707180e520c94c6d9e3de144b06cd
shack1234 <shadrack.anayo@strathmore.edu>
2020-04-12 11:31:38 +0300

    added repo class

--- src/app/repositories.ts
+++ src/app/repositories.ts
@@ -1,5 +1,11 @@
 export class Repositories {
-    constructor(public name:string,public html_url:string ,public description:string, public created_at:Date
-        , public forks:number,public language:string){
-      }
+  constructor(
+    public name: string, 
+    public html_url: string, 
+    public description: string,
+    public created_at: Date,
+    public forks: number, 
+    public language: string
+    ){
+  }
 }
